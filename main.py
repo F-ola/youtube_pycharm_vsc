91d51909cf0b57a409e162ce758de8dafde6d66f
Moses <oladipupo2rone@outlook.com>
2022-03-24 17:34:46 +0100

    We are testing new function

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def my_function(a, b):
+    return a + b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Folabi')
     print("How are you?")
     print("Where do you live?")
+
+    print(my_function(2, 4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
